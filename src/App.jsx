0ca89813f5b6948fdb93d3d5291af94c8e306775
jyothsna30-design <jyothsnapriyanka30@gmail.com>
2026-01-07 12:45:45 +0300

    added routes

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,12 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import Navbar from './Components/Navbar'
 import Home from './Components/Home'
-
+import Video from './Components/Video'
+import {Route,Routes} from 'react-router-dom'
+import Profile from './Components/Profile'
+import Videoupload from './Components/Videoupload'
+import Login from './Components/Login'
+import Signup from './Components/Signup'
 function App() {
   const [sidebar,setsidebar]= useState(true);
   const setSidebarFun = (value)=>{
@@ -12,7 +17,15 @@ function App() {
   }
  return(<div className='bg-black text-white w-full min-h-screen'>
   <Navbar setSidebarFun={setSidebarFun} sidebar={sidebar}/>
-  <Home sidebar={sidebar}/>
+  <Routes>
+    <Route path='/' element={<Home sidebar={sidebar} />}/>
+    <Route path='/watch/:id' element={<Video />} />
+    <Route path='/user/:id' element={<Profile sidebar={sidebar}/>} />
+    <Route path='/:id/upload' element={<Videoupload />} />
+    <Route path='/login' element={<Login />} />
+    <Route path='/signup' element={<Signup />} />
+  </Routes>
+  
   </div>
  )
 }
