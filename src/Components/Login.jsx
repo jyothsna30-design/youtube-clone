9f203eb836acb8ce6bb200d4b9e20e973cd741bd
jyothsna30-design <jyothsnapriyanka30@gmail.com>
2026-01-13 08:17:49 +0300

    cloudinary connect

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -1,15 +1,26 @@
 import './Login.css'
 import YouTubeIcon from '@mui/icons-material/YouTube';
+import {useState} from 'react'
 import { Link } from 'react-router-dom';
 function Login() {
+    const [loginField,setloginField] = useState({
+        username:"",
+        password:""
+    });
+    const handleOnChange = (e,field)=>{
+    setloginField({
+        ...loginField,
+        [field]:e.target.value
+    })
+    }
   return (
     <div className="login">
         <div className="login_card">
               <div className="login_card_header">
                 <YouTubeIcon sx={{color:"red",fontSize:"34px"}}> </YouTubeIcon>Login
                 </div>
-                  <input type="text" placeholder="Username"className="mb-4 p-2 mt-2 w-full text-white bg-[#222222] rounded-md "/>
-                  <input type="password" placeholder="password"className="mb-4 p-2 w-full text-white bg-[#222222] rounded-md "/>
+                  <input type="text" placeholder="Username" value={loginField.username} onChange={(e)=> handleOnChange(e,"username")} className="mb-4 p-2 mt-2 w-full text-white bg-[#222222] rounded-md "/>
+                  <input type="password" placeholder="password" value={loginField.password} onChange={(e)=> handleOnChange(e,"password")} className="mb-4 p-2 w-full text-white bg-[#222222] rounded-md "/>
                   <div className="flex gap-10 justify-between">
                   <button className="bg-red-600 text-white p-2 w-20 rounded-md hover:bg-red-800">  Login </button>
                   <Link to='/signup'><button className="bg-red-600 text-white p-2 w-20 rounded-md hover:bg-red-800">  SignUp </button></Link>
