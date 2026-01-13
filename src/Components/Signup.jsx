7e091b218a7c329ed622d6faf1c033b0f491cc64
jyothsna30-design <jyothsnapriyanka30@gmail.com>
2026-01-13 08:24:13 +0300

    cloudinary connect

--- src/Components/Signup.jsx
+++ src/Components/Signup.jsx
@@ -1,6 +1,32 @@
 import YouTubeIcon from '@mui/icons-material/YouTube';
+import axios from 'axios';
+
 import { Link } from 'react-router-dom'; 
+import {useState} from 'react';
 function Signup(){
+
+    const [channelName,setChannelName]=useState("");
+    const [userName,setUserName]=useState("");
+    const [password,setPassword]=useState("");
+    const [aboutChannel,setAboutChannel]=useState("");
+    const [profilePic,setProfilePic]=useState(uploadedPic);
+    const [uploadedPic,setUploadedPic]=useState("https://t3.ftcdn.net/jpg/11/61/33/40/360_F_1161334053_YhWMr4wDxYZlPMSsdTau11bLUM1fpbYF.jpg");
+    const uploadImage= async (e)=>{
+        const file=e.target.files[0];
+        const data=new FormData();
+        data.append('file',file);
+        data.append('upload_preset','youtube-clone');
+        try{
+            //cloudname=dimea4rmk
+             const response = await axios.post('https://api.cloudinary.com/v1_1/dimea4rmk/image/upload',data);
+             const imageUrl=response.data.url;
+             setUploadedPic(imageUrl);
+             setProfilePic(imageUrl);
+            }
+        catch(err){
+            console.log(err);
+        }
+    }
     return (<div> <div className='bg-black'>
         <div className='w-120 relative top-20 left-100 border border-gray-700 shadow-[0.5px_0.5px_8px_white] bg-black rounded-lg'>
             <div className='flex text-white text-2xl items-center justify-center pt-7'>
@@ -8,16 +34,16 @@ function Signup(){
                 SignUp
             </div>
             <div className='flex flex-col p-2'>
-                <input type="text" placeholder='Channel Name' className='w-100 bg-gray-800 text-white p-2 m-4 rounded-md outline-none'/>
-                <input type="text" placeholder='User Name' className='w-100 bg-gray-800 text-white p-2 m-4 rounded-md outline-none'/>
-                <input type="password" placeholder='Password' className='w-100 bg-gray-800 text-white p-2 m-4 rounded-md outline-none'/>
-                <input type="text" placeholder='About Your Channel' className='w-100 bg-gray-800 text-white p-2 m-4 rounded-md outline-none'/>
-                <div className='p-4 ml-6 flex'><input type='file' accept='image/*' className='ml-2 block w-full text-white file:mr-4 
+                <input type="text" placeholder='Channel Name' value={channelName} onChange={(e)=>setChannelName(e.target.value)} className='w-100 bg-gray-800 text-white p-2 m-4 rounded-md outline-none'/>
+                <input type="text" placeholder='User Name' value={userName} onChange={(e)=>setUserName(e.target.value)} className='w-100 bg-gray-800 text-white p-2 m-4 rounded-md outline-none'/>
+                <input type="password" placeholder='Password' value={password} onChange={(e)=>setPassword(e.target.value)} className='w-100 bg-gray-800 text-white p-2 m-4 rounded-md outline-none'/>
+                <input type="text" placeholder='About Your Channel' value={aboutChannel} onChange={(e)=>setAboutChannel(e.target.value)} className='w-100 bg-gray-800 text-white p-2 m-4 rounded-md outline-none'/>
+                <div className='p-4 ml-6 flex'><input type='file' onChange={(e)=>uploadImage(e)} className='ml-2 block w-full text-white file:mr-4 
       file:rounded file:border-0
       file:bg-white file:text-black
       hover:file:bg-gray-200'/></div>
               <div className='flex'>
-                <img src="https://t3.ftcdn.net/jpg/11/61/33/40/360_F_1161334053_YhWMr4wDxYZlPMSsdTau11bLUM1fpbYF.jpg" className="w-20 h-20 rounded-full ml-6"/>
+                <img src={uploadedPic} className="w-20 h-20 rounded-full ml-6"/>
                <div className='ml-10 mb-4 mt-4'>
                 <button className='bg-red-600 text-white p-2 rounded-md hover:bg-red-800'>SignUp</button>   
                 <Link to='/' className='bg-gray-600 text-white p-2 rounded-md hover:bg-gray-800 ml-4'><button>Home</button></Link>
