f63fc5535bd2e1407076c5715ce4c8a5340f2763
jyothsna30-design <jyothsnapriyanka30@gmail.com>
2026-01-18 16:36:30 +0300

    added comment

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -10,12 +10,17 @@ import Profile from './Components/Profile'
 import Videoupload from './Components/Videoupload'
 import Login from './Components/Login'
 import Signup from './Components/Signup'
+
 function App() {
   const [sidebar,setsidebar]= useState(true);
+ 
+
   const setSidebarFun = (value)=>{
     setsidebar(value)
   }
+  //  routing and navsidebar 
  return(<div className='bg-black text-white w-full min-h-screen'>
+  
   <Navbar setSidebarFun={setSidebarFun} sidebar={sidebar}/>
   <Routes>
     <Route path='/' element={<Home sidebar={sidebar} />}/>
