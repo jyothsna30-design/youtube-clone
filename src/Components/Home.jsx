791962f0b74806bbbb9df14b50e8d85d4a528a8f
jyothsna30-design <jyothsnapriyanka30@gmail.com>
2026-01-20 13:10:27 +0300

    final update

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,12 +1,12 @@
 import Homepage from "./Homepage";
 import Sidenavbar from "./Sidenavbar";
 
-function Home({sidebar}){
+function Home({sidebar,search}){
     // Home component rendering sidebar and homepage
     return(<>
     <div className="flex">
      <Sidenavbar sidebar={sidebar}/>
-     <Homepage sidebar={sidebar}/>
+     <Homepage sidebar={sidebar} search={search}/>
      </div>
     </>)
 }
