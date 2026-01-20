6c75139179e78651e02fd33d245e047accdffc82
jyothsna30-design <jyothsnapriyanka30@gmail.com>
2026-01-20 13:22:32 +0300

    final update

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,26 @@ import Signup from './Components/Signup'
 
 function App() {
   const [sidebar,setsidebar]= useState(true);
- 
+  const [search, setSearch] = useState("");
+  const [isMobile, setIsMobile] = useState(false); 
+ // when window less than 800
+  useEffect(() => {
+  const handleResize = () => {
+    if (window.innerWidth < 800) { 
+      setsidebar(false);          
+      setIsMobile(true);
+    } else {
+      setsidebar(true);           
+      setIsMobile(false);
+    }
+  }
+
+  handleResize(); 
+  window.addEventListener("resize", handleResize);
+
+  return () => window.removeEventListener("resize", handleResize);
+}, []);  
+  
 
   const setSidebarFun = (value)=>{
     setsidebar(value)
@@ -21,9 +40,9 @@ function App() {
   //  routing and navsidebar 
  return(<div className='bg-black text-white w-full min-h-screen'>
   
-  <Navbar setSidebarFun={setSidebarFun} sidebar={sidebar}/>
+  <Navbar setSidebarFun={setSidebarFun} sidebar={sidebar} search={search} setSearch={setSearch} isMobile={isMobile}/>
   <Routes>
-    <Route path='/' element={<Home sidebar={sidebar} />}/>
+    <Route path='/' element={<Home sidebar={sidebar} search={search}/>}/>
     <Route path='/watch/:id' element={<Video />} />
     <Route path='/user/:id' element={<Profile sidebar={sidebar}/>} />
     <Route path='/:id/upload' element={<Videoupload />} />
